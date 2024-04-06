f579711e57e6f05b6d55dfc0d683c8182acad478
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-04-06 17:38:01 +0900

    2차 세미나 완료!

---
+++ practice/src/main/java/org/sopt/practice/controller/dto/MemberController.java
@@ -0,0 +1,39 @@
+package org.sopt.practice.controller.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.practice.domain.Member;
+import org.sopt.practice.service.MemberService;
+import org.sopt.practice.service.dto.MemberCreateDto;
+import org.sopt.practice.service.dto.MemberFindDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity createMember(
+            @RequestBody MemberCreateDto memberCreate
+    ) {
+        return ResponseEntity.created(URI.create(memberService.createMember(memberCreate))).build();
+
+    }
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberFindDto> findMemberById(@PathVariable Long memberId){
+        return ResponseEntity.ok(memberService.findMemberById(memberId));
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMemberById(
+            @PathVariable Long memberId
+    ) {
+        memberService.deleteMemberById(memberId);
+        return ResponseEntity.noContent().build();
+    }
+}
