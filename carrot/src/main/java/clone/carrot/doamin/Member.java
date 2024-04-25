bf1d6ec59cff0d2ce7328d0372e1d4b63b3b87b1
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-04-25 15:07:08 +0900

    Member 도메인 City, age 추가

--- carrot/src/main/java/clone/carrot/doamin/Member.java
+++ carrot/src/main/java/clone/carrot/doamin/Member.java
@@ -2,16 +2,37 @@ package clone.carrot.doamin;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Member {
+
+
     @Id
     @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    private City city;
+
+    private int age;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public static Member create(
+            String name
+    ) {
+        return new Member(name);
+    }
+
+
 
 }
